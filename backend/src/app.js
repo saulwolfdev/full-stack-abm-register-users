7d9f4c646ac3a832da9fcb37458941247b52adc8
saul alejos garay <43302778+saulwolfdev@users.noreply.github.com>
2020-08-06 18:34:21 -0300

    conexion a la base

---
+++ backend/src/app.js
@@ -0,0 +1,16 @@
+  
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+// app.set('port', process.env.PORT || 4000);
+
+
+// app.use(cors());
+// app.use(express.json());
+
+// app.use('/api/notes', require('./routes/notes'));
+// app.use('/api/users', require('./routes/users'));
+
+module.exports = app;
---
+++ backend/src/index.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+
+const app=require("./app");
+require("./database");
+
+async function main() {
+	await app.listen(4000);
+	console.log("server on port 4000")
+}
+main();

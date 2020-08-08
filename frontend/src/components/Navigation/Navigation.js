a70ada2f34398584eb2b280dabbb95f8cce5cfb2
saul alejos garay <43302778+saulwolfdev@users.noreply.github.com>
2020-08-08 00:34:42 -0300

    customizacion

--- frontend/src/components/Navigation/Navigation.js
+++ frontend/src/components/Navigation/Navigation.js
@@ -1,41 +1,42 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 // import styles from "./Navigation.module.scss";
 class Navigation extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {}
-	}
-	render() {
-		return (
-
-				<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-					<div className={"container"}>
-						<Link className="navbar-brand" to="/">
-							home
-					</Link>
-						<div className="collapse navbar-collapse" id="navbarNav">
-							<ul className="navbar-nav">
-							<li className="nav-item active">
-                                <Link to="/" className="nav-link">Note LIST</Link>
-                            </li>
-								<li className="nav-item">
-									<Link className="nav-link" to="/create">
-										create note
-									</Link>
-								</li>
-								<li className="nav-item">
-									<Link className="nav-link" to="/user">
-										create user
-								</Link>
-								</li>
-							</ul>
-						</div>
-					</div>
-				</nav>
-		);
-	}
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className={"container"}>
+          <Link className="navbar-brand" to="/">
+            home
+          </Link>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item active">
+                <Link to="/" className="nav-link">
+                  Note LIST
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/create">
+                  create note
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/user">
+                  create user
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
 }
 
 export default Navigation;
